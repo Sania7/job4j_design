270ed34525d7cb8724cebab3c840093e216d174c
macbook <79824177721@yandex.ru>
2022-10-27 16:16:37 +0500

    AssertJ

--- src/test/java/ru/job4j/iterator/ArrayItTest.java
+++ src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -1,9 +1,8 @@
-package ru.job4j;
+package ru.job4j.iterator;
 
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayItTest {
     @Test
